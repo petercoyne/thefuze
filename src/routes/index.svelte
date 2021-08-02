0c4ad70ee782f88f8b551569e630895b814a7d5d
Peter Coyne <g00266507@gmit.ie>
2021-08-02 12:46:30 +0100

    First commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<div class="w-full grid grid-cols-1 grid-rows-1 max-w-screen-lg mx-auto">
+    <img src="bg1crop2.webp" alt="" class="z-0 sm:hidden" style="grid-area: 1/1/1/1; min-width: 200%; margin-left: -50%"/>
+    <img src="bg1crop2.webp" alt="" class="z-0 sm:block hidden" style="grid-area: 1/1/1/1"/>
+
+    <div class="z-20" style="grid-area: 1/1/1/1; margin-top: 10%">
+        <img src="logo.png" alt="" class="z-20 mx-auto w-2/3 sm:w-1/3 mt-8 sm:mt-0"/>
+        <nav class="mx-auto flex justify-around fuzestroke w-2/3 sm:w-1/3 text-sm">
+            <a href="#" class="flex p-2 rounded-md bg-white bg-opacity-50 hover:bg-black hover:bg-opacity-25">
+                <img src="bio.svg" alt="" class="w-5 h-5 mr-2">
+                <div class="inline">Bio</div>
+            </a>
+            <a href="#" class="flex p-2 rounded-md hover:bg-black hover:bg-opacity-25">
+                <img src="listen.svg" alt="" class="w-5 h-5 mr-2">
+                <div>Listen</div>
+            </a>
+            <a href="#" class="p-2 rounded-md hover:bg-black hover:bg-opacity-25">
+                <img src="sc.svg" alt="" class="w-7 h-5"/>
+            </a>
+            <a href="#" class="p-2 rounded-md hover:bg-black hover:bg-opacity-25">
+                <img src="insta.svg" alt="" class="w-5 h-5"/>
+            </a>
+        </nav>
+    </div>
+</div>
+<div class="max-w-screen-lg mx-auto border-t-2 border-gray-800 bg-gray-900 h-12"></div>
