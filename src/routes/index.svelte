d18afa330243536b9a1666de1d8d57ebb3b2b649
Peter Coyne <g00266507@gmit.ie>
2021-08-22 14:12:12 +0100

    tweak

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 
     <div class="z-20" style="grid-area: 1/1/1/1; margin-top: 10%">
         <img src="logo.png" alt="" class="z-20 mx-auto w-2/3 sm:w-1/3 mt-8 sm:mt-0"/>
-        <nav class="mx-auto flex justify-around items-center fuzestroke w-2/3 sm:w-1/3 text-sm lg:text-lg">
+        <nav class="mx-auto flex justify-around items-center fuzestroke w-2/3 sm:w-1/3 text-md font-semibold lg:text-xl">
             <a href="#" class="flex items-center p-2 rounded-md bg-white bg-opacity-50 hover:bg-black hover:bg-opacity-25">
                 <img src="bio.svg" alt="" class="w-5 h-5 mr-2 lg:w-7 lg:h-7">
                 <div class="inline">Bio</div>
@@ -13,37 +13,29 @@
                 <img src="listen.svg" alt="" class="w-5 h-5 mr-2 lg:w-7 lg:h-7">
                 <div>Listen</div>
             </a>
-            <a href="#" class="p-2 rounded-md hover:bg-black hover:bg-opacity-25">
+            <!-- <a href="#" class="p-2 rounded-md hover:bg-black hover:bg-opacity-25">
                 <img src="sc.svg" alt="" class="w-7 h-5 lg:w-9 lg:h-7"/>
-            </a>
+            </a> -->
             <a href="#" class="p-2 rounded-md hover:bg-black hover:bg-opacity-25">
                 <img src="insta.svg" alt="" class="w-5 h-5 lg:w-7 lg:h-7"/>
             </a>
         </nav>
     </div>
 </div>
-<div class="max-w-screen-lg mx-auto border-t-2 border-gray-600 bg-black text-white p-8 h-full"
+<div class="max-w-screen-lg mx-auto border-t-2 border-gray-600 bg-black text-white h-full"
     style="background-image: url('bg-gradient.png'); background-repeat: repeat-x;">
-    <h6 class="text-gray-500 mb-6">The Fuze : <b>Bio</b></h6>
-    <div class="flex flex-col">
-        <div>
-            <img src="band-bftp.jpg" alt=""/>
-        </div>
-        <div>
-            <span class="text-gray-200">The Fuze.</span>
-            <span class="text-gray-400 text-3xl">A no-name band outa</span>
-            <span class="text-white text-5xl font-bold ">Galway, 1979</span>
-            <span class="text-2xl">and counting.</span>
-            <span>Colleges and commerce;</span>
-            <span>art and pop; rock and punk.</span>
-            <p>Strange bedfellows</p>
-            <p>but passions were primed.</p>
-            <p>So go for it man.</p>
-        </div>
-    </div>
+    <!-- <h6 class="text-gray-500 p-8">The Fuze : <b>Bio</b></h6> -->
+    <img src="band-bftp.jpg" alt=""/>
 
-    <p>You could say it was heady, and it was. First a charity gig in the new Physics Hall. Idol virgins through sound. On into the future. The buzz man, the buzz. New Irish rock. The timing sweet and our own making it happen. Power, performance, energy. Something could explode, hear.</p>
-    <p>You've gotta remember the late seventies and early eighties - because a fierce lot can't. The whole Galway of the Vibes thing birthing free-range students from its universal Cellar Bar, Lounge or Downstairs. Pre-conscious consciousness at its purest. Seven pints and a glass of Guinness. Two pints of Smithwicks and a pint of Harp, Sean, please (to dull the stench of corruption outside). And while you're at it, four Black Russians.</p>
-    <p>And love was all around. Love of the truths shared in rocking the foundations. Coming, going, starting to leave. All the time trying to be faithful to an unhappy country.</p>
-    <p>And the band became all. Careers and studies were abandoned. PA, lights and instruments leased. Engineers and roadies hired (and highered - if you'll excuse the expression). The ballrooms of Ireland throbbwed to one, last capacity crowd. Summer of eighty-one and Sunday Morning was happening ten times a day on Radio 2. The lads gigging six nights a week from Donegal to Dublin.</p>
+    <div class="p-8 border-b border-gray-700">
+        <p class="text-gray-200 text-5xl font-bold">The Fuze.</p>
+        <p class="text-white text-4xl leading-10">A no-name band outa Galway, 1979 and counting.</p>
+        <p class="text-gray-400 text-xl pt-2 leading-8">Colleges and commerce; art and pop; rock and punk. Strange bedfellows but passions were primed. <b>So go for it man.</b></p>
+    </div>
+    <div class="p-8 pt-4">
+        <p>You could say it was heady, and it was. First a charity gig in the new Physics Hall. Idol virgins through sound. On into the future. The buzz man, the buzz. New Irish rock. The timing sweet and our own making it happen. Power, performance, energy. Something could explode, hear.</p>
+        <p>You've gotta remember the late seventies and early eighties - because a fierce lot can't. The whole Galway of the Vibes thing birthing free-range students from its universal Cellar Bar, Lounge or Downstairs. Pre-conscious consciousness at its purest. Seven pints and a glass of Guinness. Two pints of Smithwicks and a pint of Harp, Sean, please (to dull the stench of corruption outside). And while you're at it, four Black Russians.</p>
+        <p>And love was all around. Love of the truths shared in rocking the foundations. Coming, going, starting to leave. All the time trying to be faithful to an unhappy country.</p>
+        <p>And the band became all. Careers and studies were abandoned. PA, lights and instruments leased. Engineers and roadies hired (and highered - if you'll excuse the expression). The ballrooms of Ireland throbbwed to one, last capacity crowd. Summer of eighty-one and Sunday Morning was happening ten times a day on Radio 2. The lads gigging six nights a week from Donegal to Dublin.</p>    
+    </div>
 </div>
