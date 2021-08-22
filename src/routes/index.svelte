72dd3296e481affeaecc10d166d99f87b5611108
Peter Coyne <g00266507@gmit.ie>
2021-08-22 13:36:50 +0100

    not sure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,7 +24,26 @@
 </div>
 <div class="max-w-screen-lg mx-auto border-t-2 border-gray-600 bg-black text-white p-8 h-full"
     style="background-image: url('bg-gradient.png'); background-repeat: repeat-x;">
-    <h6 class="text-gray-500">The Fuze : <b>Bio</b></h6>
-    <h2 class="text-gray-200">The Fuze were a band from the west of Ireland</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum error ipsam obcaecati expedita earum soluta veritatis quaerat aliquam facilis ipsum temporibus, laborum saepe debitis ad. Inventore, hic natus. Suscipit, reiciendis?</p>
+    <h6 class="text-gray-500 mb-6">The Fuze : <b>Bio</b></h6>
+    <div class="flex flex-col">
+        <div>
+            <img src="band-bftp.jpg" alt=""/>
+        </div>
+        <div>
+            <span class="text-gray-200">The Fuze.</span>
+            <span class="text-gray-400 text-3xl">A no-name band outa</span>
+            <span class="text-white text-5xl font-bold ">Galway, 1979</span>
+            <span class="text-2xl">and counting.</span>
+            <span>Colleges and commerce;</span>
+            <span>art and pop; rock and punk.</span>
+            <p>Strange bedfellows</p>
+            <p>but passions were primed.</p>
+            <p>So go for it man.</p>
+        </div>
+    </div>
+
+    <p>You could say it was heady, and it was. First a charity gig in the new Physics Hall. Idol virgins through sound. On into the future. The buzz man, the buzz. New Irish rock. The timing sweet and our own making it happen. Power, performance, energy. Something could explode, hear.</p>
+    <p>You've gotta remember the late seventies and early eighties - because a fierce lot can't. The whole Galway of the Vibes thing birthing free-range students from its universal Cellar Bar, Lounge or Downstairs. Pre-conscious consciousness at its purest. Seven pints and a glass of Guinness. Two pints of Smithwicks and a pint of Harp, Sean, please (to dull the stench of corruption outside). And while you're at it, four Black Russians.</p>
+    <p>And love was all around. Love of the truths shared in rocking the foundations. Coming, going, starting to leave. All the time trying to be faithful to an unhappy country.</p>
+    <p>And the band became all. Careers and studies were abandoned. PA, lights and instruments leased. Engineers and roadies hired (and highered - if you'll excuse the expression). The ballrooms of Ireland throbbwed to one, last capacity crowd. Summer of eighty-one and Sunday Morning was happening ten times a day on Radio 2. The lads gigging six nights a week from Donegal to Dublin.</p>
 </div>
