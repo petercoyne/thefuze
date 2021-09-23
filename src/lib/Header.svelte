f7d21ce97e9b5d2b3ae5a11ab0776c3cf9aa8973
Peter Coyne <g00266507@gmit.ie>
2021-09-23 15:46:48 +0100

    change header bg

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
-<div class="w-full max-w-full bg-cover bg-repeat-x lg:bg-contain bg-center flex flex-col items-center justify-around"
-style="background-image: url('/bg1crop.webp')">
+<div class="w-full max-w-full h-screen bg-cover bg-repeat-x bg-center flex flex-col items-center justify-around"
+style="background-image: url('/bg2.webp')">
     <img src="/logo.png" alt="" class="w-2/3 sm:w-1/2 lg:w-1/3 xl:w-1/4 my-36 xl:my-48"/>
     <a href="#maincontent">
         <svg class="bg-white bg-opacity-70 rounded-full w-10 h-10 mb-8 sm:mb-16 p-2 z-30 mx-auto animate-bounce text-black"
